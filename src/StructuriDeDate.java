6766b0174c788388adced4c74356142aa20d8021
robert.ban <robertban80@gmsil.com>
2023-01-17 12:17:34 +0200

    rezolvare ex 13 tema lab 28 - 16.01.2023

--- src/StructuriDeDate.java
+++ src/StructuriDeDate.java
@@ -3,30 +3,30 @@ import java.util.HashMap;
 
 public class StructuriDeDate {
     public static void main(String[] args){
-        ArrayList<String> listaNoastra = new ArrayList<>();
-        listaNoastra.add("First");
-        listaNoastra.add("Second");
-        listaNoastra.set(0,"First-updated");
-        //listaNoastra.remove(0);
-        System.out.println(listaNoastra.get(0));
-        System.out.println(listaNoastra.indexOf("Second"));
-
-        for (String value: listaNoastra){
-            System.out.println(value);
-
-        }
-
-        HashMap <Integer, String> dictionarulNostru = new HashMap<>();
-        dictionarulNostru.put(1,"Primul");
-        dictionarulNostru.put(2,"Secund");
-        dictionarulNostru.put(3,"Third");
-
-        System.out.println(dictionarulNostru.get(0));
-        System.out.println(dictionarulNostru.keySet());
-        System.out.println(dictionarulNostru.values());
-        System.out.println(dictionarulNostru.entrySet());
-        dictionarulNostru.replace(1,"Noua valoare");
-        System.out.println(dictionarulNostru.entrySet());
+//        ArrayList<String> listaNoastra = new ArrayList<>();
+//        listaNoastra.add("First");
+//        listaNoastra.add("Second");
+//        listaNoastra.set(0,"First-updated");
+//        //listaNoastra.remove(0);
+//        System.out.println(listaNoastra.get(0));
+//        System.out.println(listaNoastra.indexOf("Second"));
+//
+//        for (String value: listaNoastra){
+//            System.out.println(value);
+//
+//        }
+//
+//        HashMap <Integer, String> dictionarulNostru = new HashMap<>();
+//        dictionarulNostru.put(1,"Primul");
+//        dictionarulNostru.put(2,"Secund");
+//        dictionarulNostru.put(3,"Third");
+//
+//        System.out.println(dictionarulNostru.get(0));
+//        System.out.println(dictionarulNostru.keySet());
+//        System.out.println(dictionarulNostru.values());
+//        System.out.println(dictionarulNostru.entrySet());
+//        dictionarulNostru.replace(1,"Noua valoare");
+//        System.out.println(dictionarulNostru.entrySet());
 
 // tema lab 28 - 16.01.2023
         HashMap <String, Boolean> cineTreceExamenul = new HashMap<>();
@@ -49,5 +49,14 @@ public class StructuriDeDate {
             if (cineTreceExamenul.get(value) == true) {System.out.println(value);}
 
         }
+//varianta Cristina
+        for (String nume: cineTreceExamenul.keySet()){
+            if (cineTreceExamenul.get(nume) == true) {
+                System.out.println("key: " + nume + " value: " + cineTreceExamenul.get(nume));
+            }
+
+        }
     }
+
+
 }
